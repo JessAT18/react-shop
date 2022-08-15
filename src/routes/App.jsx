692d35c33c804bd7fd12012c03f86864ffaeaaf7
JessAT18 <j-aquino@outlook.com>
2022-08-15 16:41:17 -0400

    Solucionando el reto...

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Layout from '../containers/Layout';
 import Login from '../containers/Login';
-import RecoveryPassword from '../containers/RecoveryPassword';
+import PasswordRecovery from '../containers/PasswordRecovery';
 import Home from '../pages/Home';
 import NotFound from '../pages/NotFound';
 import '../styles/global.css'
@@ -14,7 +14,7 @@ const App = () => {
                 <Routes>
                     <Route exact path='/' element={<Home />} />
                     <Route exact path='/login' element={<Login />}/>
-                    <Route exact path='/recovery-password' element={<RecoveryPassword />}/>
+                    <Route exact path='/recovery-password' element={<PasswordRecovery />}/>
                     <Route path='*' element={<NotFound />} />
                 </Routes>
             </Layout>
