3cc796c71064472212e17467ed7a33a1cb97cb97
JessAT18 <j-aquino@outlook.com>
2022-08-15 15:19:52 -0400

    React Router DOM

--- src/containers/Login.jsx
+++ src/containers/Login.jsx
@@ -8,13 +8,13 @@ const Login = () => {
             <img src="./logos/logo_yard_sale.svg" alt="logo" className="logo"/>
         
             <h1 className="title">Create a new password</h1>
-            <p classNameName="subtitle">Enter a new password for your account</p>
+            <p className="subtitle">Enter a new password for your account</p>
         
             <form action="/" className="form">
-                <label for="password" className="label">Password</label>
+                <label htmlFor="password" className="label">Password</label>
                 <input type="password" id="password" placeholder="*********" className="input input-password"/>
         
-                <label for="new-password" className="label">Password</label>
+                <label htmlFor="new-password" className="label">Password</label>
                 <input type="password" id="new-password" placeholder="*********" className="input input-password"/>
         
                 <input type="submit" value="Confirm" className="primary-button login-button"/>
